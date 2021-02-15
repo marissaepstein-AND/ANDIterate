15dd33a78914b17522b166fc57d857067ddfff68
Sam West <sam.west@and.digital>
2021-02-15 16:06:10 +0000

    Added infoBox template

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { media } from "../../theme"
 import styled from "styled-components"
 import { Page } from "../common/Page"
+import InfoBox from "../common/InfoBox";
 
 
 const Title = styled.div`
@@ -10,7 +11,32 @@ const Title = styled.div`
 
 const Home = () => {
 
-    return <Page><Title>Hello</Title></Page>
+    //Create contents for the sprint goal info box
+    const sprintGoalProps ={
+        title: "Sprint goals",
+        body: 
+            <ul>
+                <li>Set up </li>
+                <li></li>
+            </ul>,
+        style:{
+            wrapperStyle:{
+
+            },
+            titleStyle:{
+                backgroundColor:'#FF323C',
+            },
+            bodyStyle:{
+
+            }
+        }
+        
+    }
+
+    return <Page>
+      <Title>Hello</Title>
+      <InfoBox {...sprintGoalProps} />
+    </Page>
 }
 
 export default Home
