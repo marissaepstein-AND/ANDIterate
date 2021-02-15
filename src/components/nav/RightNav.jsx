d88e2ff945172271dcec3e662a0b9cdc0634cdbe
Marissa Epstein (AND Digital) <79081496+marissaepstein-AND@users.noreply.github.com>
2021-02-15 16:08:06 +0000

    navbar added

---
+++ src/components/nav/RightNav.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from "../../theme"
+
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+
+  li {
+    padding: 0 1.5rem;
+    font-weight: ${p => p.theme.h1.fontWeight};
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: ${p => p.theme.colors.red};
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    margin: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+      padding: 1rem 0;
+      font-size: ${p => p.theme.h1.size};
+      width: 90%;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li>Home</li>
+      <li>Scrum Hints & Tips</li>
+    </Ul>
+  )
+}
+
+export default RightNav
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,18 +4,25 @@ import { Switch, Route, HashRouter } from 'react-router-dom'
 // import axios from 'axios'
 import '../styles/App.css';
 import firebase from "firebase";
+import theme from "../theme"
+
 import Home from '../components/pages/Home'
+import Menu from "../components/nav/Menu"
+import { ThemeProvider } from 'styled-components';
 
 const App = (props) =>{
 
   const firebaseApp = firebase.apps[0];
 
   return (
+    <ThemeProvider theme={theme}>
     <HashRouter>
+      <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
     </HashRouter>
+</ThemeProvider>
   );
 }
 
