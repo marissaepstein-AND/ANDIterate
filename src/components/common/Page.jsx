4b439049b1a3b7ae99ebbfef9c57fcc293d6477d
Marissa Epstein (AND Digital) <79081496+marissaepstein-AND@users.noreply.github.com>
2021-02-16 15:50:08 +0000

    added cards to home

--- src/components/common/Page.jsx
+++ src/components/common/Page.jsx
@@ -1,8 +1,16 @@
 import styled, { css } from "styled-components";
+import { media } from "../../styles/theme"
 
 
 export const Page = styled.div`
+    display: flex; 
+    flex-direction: column;
     margin: 0 auto;
     padding: 1rem;
     min-height: -webkit-fill-available;
+
+    ${media.tablet`
+    flex-direction: row;
+
+    `}
 `
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,29 +2,63 @@ import React from 'react'
 import { media } from "../../styles/theme"
 import styled from "styled-components"
 import { Page } from "../common/Page"
-import InfoBox from "../common/InfoBox";
-import TeamMembers from '../common/TeamMembers';
-import TeamBox from "../common/TeamBox";
+// import InfoBox from "../common/InfoBox";
+// import TeamMembers from '../common/TeamMembers';
+// import TeamBox from "../common/TeamBox";
 import SprintGoalCard from "../common/cards/sprintGoalCard";
 import RetroActionsCard from '../common/cards/retroActionsCard';
 import DodCard from '../common/cards/dodCard';
 import DorCard from '../common/cards/dorCard';
+import EventCard from "../common/cards/SprintEvent"
 
+const LeftContainer = styled.div`
+    width: 100%;
+
+    ${media.tablet`
+        width: 50%;
+    `}
+
+`
+
+const RightContainer = styled.div`
+    width: 100%; 
+
+    ${media.tablet`
+     width: 50%;
+     display: flex; 
+     flex-direction: column;
+`}
+`
+
+const Top = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    ${media.mobileLarge`
+        flex-direction: row;
+    `}
+`
 
 
 
 const Home = () => {
 
     //Create contents for the sprint goal info box
-    
 
     return <Page>
-    
-      <SprintGoalCard/>
-      <RetroActionsCard />
-      <DodCard />
-      <DorCard />
+        <LeftContainer>
+            <EventCard />
+            <SprintGoalCard/>
+        </LeftContainer>
+        <RightContainer>
+            <Top>
+            <DodCard />
+            <DorCard />
+            </Top>
 
+            <RetroActionsCard />
+
+      </RightContainer>
       
     </Page>
 }
