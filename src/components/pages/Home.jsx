a904526eae17c88dcb1f93b9a5216375971da56f
Ryan <ryan.gilbert@and.digital>
2021-02-16 15:20:37 +0000

    Added a mimicking api call to the Calendar API.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Page } from "../common/Page"
 import InfoBox from "../common/InfoBox";
 import TeamMembers from '../common/TeamMembers';
 import TeamBox from "../common/TeamBox";
+import EventBox from "../common/EventBox";
 import SprintGoalCard from "../common/cards/sprintGoalCard";
 import RetroActionsCard from '../common/cards/retroActionsCard';
 import DodCard from '../common/cards/dodCard';
@@ -24,6 +25,7 @@ const Home = () => {
       <RetroActionsCard />
       <DodCard />
       <DorCard />
+      <EventBox />
 
       
     </Page>
