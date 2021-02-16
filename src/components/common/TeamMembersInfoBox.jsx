8d5989df423958cc33d20545f6b739ca3a247c50
Yna Ramos <yna.ramos@and.digital>
2021-02-16 16:10:05 +0000

    Responsive display of members' details

--- src/components/common/TeamMembersInfoBox.jsx
+++ src/components/common/TeamMembersInfoBox.jsx
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import TeamMembers from '../data/teams'
+
 /* STYLED COMPONENTS */
-const MemberRow = styled.div`
-    display: flex;
-    flex-direction: row;
-    border-style: solid;
-    border-radius: 10px;
-    border-color: black;
-    border-width: 2px;
+const Card = styled.div`
+    padding: 30px;
+    text-align: center;
 `
 
-const Card = styled.div`
-        padding: 15px;
-        text-align: center;
+const MemberImage = styled.img`
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+`
+const MemberRow = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    border-style: solid;
+    border-radius: 20px;
+    border-color: ${p => p.theme.colors.secondaryPurple};
+    border-width: 2px;
 `
 /* STYLED COMPONENTS END */
 
+
 const MemberCard = (m) => {
+
     return (
         <Card>
+            <MemberImage src={m.photo} />
             <h2>{m.name}</h2>
             <h4>{m.role}</h4>
             <h4><a href={'mailto:' + m.email}>{m.email}</a></h4>
@@ -27,9 +37,11 @@ const MemberCard = (m) => {
     );
 }
 const TeamMembersInfoBox = (props) => {
+    const [members, setMembers] = useState(TeamMembers)
+
     return (
         <MemberRow>
-            {TeamMembers.map(member => (
+            {members.map(member => (
                 <MemberCard {...member} />
             ))}
         </MemberRow>
--- src/components/data/teams.js
+++ src/components/data/teams.js
@@ -1,3 +1,4 @@
+import pigeon from "../../assets/images/pigeon.jpg"
 const teamMembers =
     [
         {
@@ -5,49 +6,49 @@ const teamMembers =
             "name": "Jess Torri",
             "email": "J_AND_Digital@Example.Com",
             "role": "Scrum Master",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 2,
             "name": "Bronte Hampton",
             "email": "B_AND_Digital@Example.Com",
             "role": "Product Developer",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 3,
             "name": "Marissa Epstein",
             "email": "M_AND_Digital@Example.Com",
             "role": "Product Developer",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 4,
             "name": "Sam West",
             "email": "S_AND_Digital@Example.Com",
             "role": "Product Developer",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 5,
             "name": "Yna Ramos",
             "email": "Y_AND_Digital@Example.Com",
             "role": "Product Developer",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 6,
             "name": "Ryan Gilbert",
             "email": "R_AND_Digital@Example.Com",
             "role": "Product Developer",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         },
         {
             "id": 7,
             "name": "Toyin Oyebade",
             "email": "T_AND_Digital@Example.Com",
             "role": "Product Owner",
-            "photo": "pigeon.jpg"
+            "photo": pigeon
         }
     ]
 
