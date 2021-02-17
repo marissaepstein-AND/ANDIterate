9c7f8da5c74a46bfbd2da63f49b9117ec7b99db9
Brontë Hampton <78971989+bhampton13@users.noreply.github.com>
2021-02-17 12:28:10 +0000

    fixing import error

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-import React from 'react';
-import { media } from "../../styles/theme";
-import styled from "styled-components";
-import { Page } from "../common/Page";
-=======
 import React from 'react'
 import { media } from "../../styles/theme"
 import styled from "styled-components"
 import TeamMembersInfoBox from "../common/TeamMembersInfoBox"
 import { Page } from "../common/Page"
->>>>>>> develop
-import InfoBox from "../common/InfoBox";
+// import InfoBox from "../common/InfoBox";
 import SprintGoalCard from "../common/cards/sprintGoalCard";
 import RetroActionsCard from '../common/cards/retroActionsCard';
 import DodCard from '../common/cards/dodCard';
 import DorCard from '../common/cards/dorCard';
-import EventCard from "../common/cards/SprintEvent";
+import EventCard from "../common/cards/SprintEvent"
 
 const LeftContainer = styled.div`
     width: 100%;
